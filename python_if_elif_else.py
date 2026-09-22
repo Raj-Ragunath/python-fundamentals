c15e4273ea8464fd5de0b1d192a65088a08ae8c8
Raj Ragunath <74803676+Raj-Ragunath@users.noreply.github.com>
2026-09-21 21:13:40 -0500

    Update python_if_elif_else.py

--- python_if_elif_else.py
+++ python_if_elif_else.py
@@ -21,6 +21,8 @@ If the condition is True, the code inside the block will execute.
 """
 
 # Example - If Statements
+print("If Example: ")
+print("x = 10. If x is greater than 5, print then x is greater than 5")
 # Assign a value to x, in this case the value is 10. 
 x = 10
 
@@ -28,4 +30,56 @@ x = 10
 if x > 5:
 	# This block of code executes only if the above condition is true. 
 	print("x is greater than 5") # Output: x is greater than 5.
+"""
+Explanation:
+x = 10 : Set the variable x to 10
+if x > 5: Check if x is greater than 5
+print(...): Since x is indeed greater than 5, the message "x is greater than 5" is printed to the console. 
+"""
 
+"""
+The else Statement
+The else statement complements the if statement and specifies a block of code to be executed if the if condition is false.
+
+Syntax:
+
+if condition:
+	# Block of code to execute if the condition is true.
+else:
+	# Block of code to execute if the condition is false.
+
+Notice that the else block only executes when the if statement's condition evaluates to False.
+"""
+
+# Example - else Statements
+
+print()
+print("Else Example")
+print("x = 3. If x is greater than 5, print 'x is greater than 5', else 'print x is not greater than 5'")
+
+x = 3
+
+if x > 5:
+	print("x is greater than 5")
+else:
+	print("x is not greater than 5")
+"""
+Explanation:
+x = 3: Set the variable x to 3.
+if x > 5: Check if x is greater than 5.
+else: Since x is not greater than 5 (3 is not greater than 5), the else block executes.
+print(...) : Prints "x is not greater than 5".
+""" 
+
+"""
+The elif Statement
+The elif (else if) statement allows your to check multiple expressions for True and execute a block of code as soon as one of the conditions evaluates to True.
+
+Syntax:
+if condition1:
+	# Block of code to execute if condition1 evaluates to true.
+elif condition2:
+	# Block of code to execute if condition2 is true.
+else: 
+	# Block of code to execute if all conditions are false.
+ 
